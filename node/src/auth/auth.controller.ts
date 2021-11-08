da872c0a030558ae9b2bfd93bed52463e5692242
bsmirnov <smrnvbrs@gmail.com>
2021-11-09 02:54:28 +0300

    test

--- node/src/auth/auth.controller.ts
+++ node/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto';
 import { SignInDto } from './dto/sign-in.dto';
@@ -7,8 +7,8 @@ import { SignInDto } from './dto/sign-in.dto';
 export class AuthController {
     constructor (private readonly authService: AuthService) { }
 
-    @Post('signIn')
-    async siginIn(@Body() siginInDto: SignInDto) {
+    @Post('SignOut')
+    async siginIn(@Param() siginInDto: SignInDto) {
         return await this.authService.signIn(siginInDto)
     }
 
